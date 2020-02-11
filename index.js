314ba2cb722539fcc3007cc29fb841e0a635a094
vjk9535 <vj27021995@gmail.com>
2020-02-11 12:03:26 +0530

    mongoose e3

--- index.js
+++ index.js
@@ -9,16 +9,31 @@ connect.then((db) => {
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'vijaykumar',
+        name: 'vijaykumar1',
         description: 'test'
     })
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'},
+        },{ 
+            new: true 
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeeling!',
+            author: 'Lemonad di '
+        });
+
+        return dish.save();
+    })
+    .then((dish) =>{
+        console.log(dish);
 
         return Dishes.remove({});
     })
